67c9168090ebabf7083fa2be96b6379ea4e572d3
Gregory Subero <suberog010@gmail.com>
2024-02-13 23:01:53 -0400

    cambios en javascritp

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,8 @@
-
 let buscarForm = document.getElementById("buscador_form");
 let buttonBuscar = document.getElementById("buscador_submit");
 let buscarInput = document.getElementById("buscar_input");
-let resultsDiv = document.getElementById("results")
 
-buttonBuscar.addEventListener("click", buscador)
+buscarInput.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
 
-function buscador(){
-    const infoInput = buscarInput.value;
-    console.log(infoInput);
-
-    buscarForm.addEventListener("submit", (event) =>{
-        event.preventDefault();
-    });
-    let filer = buscarInput.value.toUpprCase();
-    let li = results.getElementsByTagName("li");
-
-    //Recorriendo elementos 
-    for (let i = 0; i < li.length; i++);{
-    }
-}
