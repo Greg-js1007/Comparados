d4aa405047ade5c92add1e1d4953981a58763a85
Gregory Subero <suberog010@gmail.com>
2024-02-15 23:57:03 -0400

    cambios del html comparar

--- src/js/app.js
+++ src/js/app.js
@@ -9,8 +9,8 @@ let nameProduct2 = document.getElementById("cart_nameProduct2").textContent;
 let nameMarket1 = document.getElementById("cart_nameProveedor").textContent;
 let nameMarket2 = document.getElementById("cart_nameProveedor2").textContent;
 
-let imageFigure = document.getElementById("cart_figure");
-let imageFigure2 = document.getElementById("cart_figure2");
+let imageFigure = document.getElementById("image_product1");
+let imageFigure2 = document.getElementById("image_product2");
 
 let buttonReiniciar = document.getElementById("reiniciar_comparados");
 
@@ -25,9 +25,12 @@ let priceHarina1 = 1.09;
 //El plazas
 let priceHarina2 = 1.11;
 
+//Hace que el cuando le des click al boton no se recargue la pagina 
 buttonComparar.addEventListener("click", (e) => {
     e.preventDefault();
 });
+
+//Esuchaadores de Eventos, hacen que cuando escuchen un click en la pagina llamen a una funcion 
 buttonComparar.addEventListener("click", comparar);
 buttonReiniciar.addEventListener("click", reiniciar);
 
@@ -36,12 +39,14 @@ function comparar(){
         mensajeResultado.innerHTML = "El producto  tiene el mismo precio en ambos establecimientos";
     }
     else if(priceHarina1 < priceHarina2){
-        mensajeResultado.innerHTML = "La " + nameProduct1 + " es mas barata en " + nameMarket1;
+        mensajeResultado.innerHTML = "La " + nameProduct1 + " es más barata en " + nameMarket1;
         imageFigure.style.backgroundColor = "#05668d";
+        mensajeResultado.style.fontWeight = "700"
     }
     else if (priceHarina1 > priceHarina2){
-        mensajeResultado.innerHTML = "La " + nameProduct2 + " es mas barata en " + nameMarket2;
+        mensajeResultado.innerHTML = "La " + nameProduct2 + " es más barata en " + nameMarket2;
         imageFigure.style.backgroundColor = "#05668d"; 
+        mensajeResultado.style.fontWeight = "700"
     }
     else{
         alert("No se pudo comparar los productos, vuelva a intentarlo");
