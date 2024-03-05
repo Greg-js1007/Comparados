16be88870173a6cc12414d98f0af5cbc26e80aea
Gregory Subero <suberog010@gmail.com>
2024-03-04 23:40:44 -0400

    Editando la vista header y el controlador Php

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -1,7 +1,7 @@
 <?php 
 
-include_once ("");
-include_once("");
-include_once("");
+include_once ("app\views\header-index.html");
+include_once("app\views\body-index.html");
+include_once("app\views\footer-index.html");
 
 ?>
