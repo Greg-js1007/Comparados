d9a7687eb5b3e5a62e4c9e1d50e8681a7d522904
Gregory Subero <suberog010@gmail.com>
2024-03-06 19:23:54 -0400

    cambiando rutas php views

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -1,7 +1,7 @@
 <?php 
 
-include_once ("../app/views/header-index.html");
-include_once("../app/views/body-index.html");
-include_once("../app/views/footer-index.html");
+include_once ("/app/views/header-index.html");
+include_once("/app/views/body-index.html");
+include_once("/app/views/footer-index.html");
 
 ?>
